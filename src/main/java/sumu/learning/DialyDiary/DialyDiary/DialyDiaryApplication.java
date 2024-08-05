3cea4709e4b124a0ff58bf1ff3e4d62c23405ba3
Sumanth <sumanth@gmail.com>
2024-08-05 17:39:53 +0530

    first commit

---
+++ src/main/java/sumu/learning/DialyDiary/DialyDiary/DialyDiaryApplication.java
@@ -0,0 +1,13 @@
+package sumu.learning.DialyDiary.DialyDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialyDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DialyDiaryApplication.class, args);
+	}
+
+}
